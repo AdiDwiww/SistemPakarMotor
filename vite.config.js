87298fa6f3d646780bc703ae6ab2406cf10d4f2c
adikhacili <adikhacili@gmail.com>
2026-05-21 22:08:43 +0700

    Configure GitHub Actions deployment for Vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MotorCheck/',
+  base: '/SistemPakarMotor/',
   build: {
-    outDir: 'MotorCheck',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 })
